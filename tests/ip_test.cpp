f3a25c4b5b49335cce05625953e70fc261532548
stryku <stryku2393@gmail.com>
2023-12-10 00:22:11 +0100

    Added SSRR tests

--- tests/ip_test.cpp
+++ tests/ip_test.cpp
@@ -826,4 +826,77 @@ TEST_F(IpTest, OptionsReader_Ssrr) {
   EXPECT_FALSE(reader.possibly_has_options());
 }
 
+TEST_F(IpTest, OptionsReader_Ssrr_TooBigLength) {
+
+  auto header = make_default_header();
+
+  // No-op
+  header.m_options[0] = option::k_strict_source_routing.to_uint8();
+  header.m_options[1] = 16; // Length, too big
+  header.m_options[2] = 4;  // Pointer, 1-indexed
+  // Address
+  header.m_options[3] = 0x11;
+  header.m_options[4] = 0x22;
+  header.m_options[5] = 0x33;
+  header.m_options[6] = 0x44;
+
+  header.m_version_and_length.m_internet_header_length += 2;
+
+  write_header(header);
+
+  const auto got_header = read_internet_header(m_buffer);
+  ASSERT_TRUE(got_header.has_value());
+
+  options_reader reader{got_header.value()};
+  ASSERT_TRUE(reader.possibly_has_options());
+  const auto got_option = reader.try_read_next();
+  ASSERT_FALSE(got_option.has_value());
+  EXPECT_EQ(got_option.error(), option_reading_error::no_enough_data);
+
+  EXPECT_FALSE(reader.possibly_has_options());
+}
+
+TEST_F(IpTest, OptionsReader_Ssrr_MalformedPointer) {
+
+  for (std::uint8_t pointer = 0; pointer < 4; ++pointer) {
+    test_print(fmt::format("pointer={}", pointer));
+
+    auto header = make_default_header();
+
+    // No-op
+    header.m_options[0] = option::k_strict_source_routing.to_uint8();
+    header.m_options[1] = 7; // Length
+    header.m_options[2] = pointer;
+    // Address
+    header.m_options[3] = 0x11;
+    header.m_options[4] = 0x22;
+    header.m_options[5] = 0x33;
+    header.m_options[6] = 0x44;
+
+    header.m_version_and_length.m_internet_header_length += 2;
+
+    write_header(header);
+
+    const auto got_header = read_internet_header(m_buffer);
+    ASSERT_TRUE(got_header.has_value());
+
+    options_reader reader{got_header.value()};
+    {
+      ASSERT_TRUE(reader.possibly_has_options());
+      const auto got_option = reader.try_read_next();
+      ASSERT_TRUE(got_option.has_value());
+      ASSERT_EQ(got_option.value().m_type, option::k_strict_source_routing);
+      EXPECT_EQ(got_option.value().m_data.size(), 5); // Including pointer.
+
+      const auto read_lsrr =
+          options_reader::decode_source_routing(got_option->m_data);
+      ASSERT_FALSE(read_lsrr.has_value());
+      EXPECT_EQ(read_lsrr.error(),
+                option_reading_error::malformed_pointer_value);
+    }
+
+    EXPECT_TRUE(reader.possibly_has_options());
+  }
+}
+
 } // namespace cool_protocols::ip::test
